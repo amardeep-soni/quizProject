81af48463d14ac68dd31ffe1999ee0c885bf9875
Amardeep soni <amardeep10as@gmail.com>
2022-10-27 17:02:40 +0545

    separeted the nav and added logic for which nav item active

--- quiz.php
+++ quiz.php
@@ -3,7 +3,7 @@ session_start();
 if (!isset($_SESSION['unique_id'])) {
     header("Location: login.php");
 }
-
+$currentPage = "quiz";
 
 ?>
 <!DOCTYPE html>
@@ -20,26 +20,9 @@ if (!isset($_SESSION['unique_id'])) {
 </head>
 
 <body id="quizBody">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto nav-pills">
-                <li class="nav-item active">
-                    <a class="nav-link" href="quiz.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="score.php">Score</a>
-                </li>
-            </ul>
-            <div class="navbar-text">
-                <a href="php/logout.php">Logout</a>
-            </div>
-        </div>
-    </nav>
+    <?php include_once 'nav.php' ?>
+
     <div class="cont">
         <div class="quiz-container p-4" id="quiz">
             <div id="error-txt">This is an error message!</div>
--- score.php
+++ score.php
@@ -4,6 +4,7 @@ if (!isset($_SESSION['unique_id'])) {
     header("Location: login.php");
 }
 $unique_id = $_SESSION['unique_id'];
+$currentPage = "score";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,26 +21,7 @@ $unique_id = $_SESSION['unique_id'];
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link" href="quiz.php">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="score.php">Score</a>
-                </li>
-            </ul>
-            <div class="navbar-text">
-                <a href="php/logout.php">Logout</a>
-            </div>
-        </div>
-    </nav>
+    <?php include_once 'nav.php' ?>
 
     <div id="scoreCont">
         <div id="score-cont">
